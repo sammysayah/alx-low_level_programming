b389fb4f72c15a294f7955e38ba21953f540ffe5
sammysayah <sayahsamson@gmail.com>
2023-09-09 09:55:39 -0700

    size of data tayp using c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,16 +4,16 @@
  * Return: 0(success)
  */
 int main(void)
-{       
-        char a;
-        int b;
-        long int c;
-        long long int d;
-        float f;
-        printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(a));
-	 printf("Size of a int : %lu byte(s)\n",(unsigned long)sizeof(b));
-	  printf("Size of a long: %lu byte(s)\n",(unsigned long)sizeof(c));
-	   printf("Size of a long long: %lu byte(s)\n",(unsigned long)sizeof(d));
-	    printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
-	    return (0);
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("size of long int : %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeod(d));
+	printf("size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
+	return (0);
 }

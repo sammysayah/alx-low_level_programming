256000787c4953f20f00283d9d015e2303eb7b72
sammysayah <sayahsamson@gmail.com>
2023-09-11 05:02:52 -0700

    commit for if statement  for positive ,negative or zero value"

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -1,11 +1,10 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-
 /**
- * main - main function of our program
- *
- * Return: Always 0 (Success)
+ * main - Main function where our program start execulting
+ * Description: output  if a random number is positive or negative.
+ * Return: 0. Program execution is a success
  */
 int main(void)
 {
@@ -13,13 +12,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	if (n == 0)
-		printf("%d is zero\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
+	if (n > 0)
+	{
 		printf("%d is positive\n", n);
-
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }

20beb4729cd2c0dce1a39e7672d0e8f458da5381
sammysayah <sayahsamson@gmail.com>
2023-09-15 08:52:07 -0700

    printing a line in terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,19 +1,14 @@
 #include "main.h"
 
 /**
- * print_line -This program uses  a function that draws a straight line in the terminal.
- * @n: An input integer
+ * print_line -This program will  draw a straight line in the terminal
+ * @n: size of line
  *
  * Return: void
  */
 void print_line(int n)
 {
-	int i = 0;
-
-	if (n > 0)
-	{
-		for (; i < n; i++)
-			_putchar('_');
-	}
+	while (--n >= 0)
+		_putchar('_');
 	_putchar('\n');
 }
